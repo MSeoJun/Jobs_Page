70f933ce6af546848b982b5babc18fd1e2195707
woobinslave <mun3582w@naver.com>
2020-09-23 19:44:39 +0900

    구글 지도 조회 로직 추가

--- components/Map.js
+++ components/Map.js
@@ -1,21 +1,39 @@
 import React from "react";
 import MapView from "react-native-maps";
+import * as API from "../Api/Works";
 
-function Maps() {
+function Maps({ address }) {
+  const [location, setLocation] = useState({
+    latitude: 0,
+    longitude: 0,
+  });
+
+  useEffect(() => {
+    const getPoint = async () => {
+      const points = await API.findAddress(address);
+      setLocation({
+        latitude: points.documents[0].y,
+        longitude: points.documents[0].x,
+      });
+    };
+
+    getPoint();
+  }, []);
+
   return (
     <MapView
       style={{ flex: 1 }}
       initialRegion={{
-        latitude: 37.4948374212982,
-        longitude: 126.73714377398,
+        latitude: location.latitude,
+        longitude: location.longitude,
         latitudeDelta: 0.009,
         longitudeDelta: 0.004,
       }}
     >
       <MapView.Marker
         coordinate={{
-          latitude: 37.4948374212982,
-          longitude: 126.73714377398,
+          latitude: location.latitude,
+          longitude: location.longitude,
         }}
       />
     </MapView>
